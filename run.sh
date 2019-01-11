d2c56ef0bf592780cec4e2706b312a2dc574cb40
hhansen06 <hhansen06@googlemail.com>
2019-01-11 20:43:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "usage: <import|run>"
-    echo "commands:"
-    echo "    import: Set up the database and import /data.osm.pbf"
-    echo "    run: Runs Apache and renderd to serve tiles at /tile/{z}/{x}/{y}.png"
-    echo "environment variables:"
-    echo "    THREADS: defines number of threads used for importing / tile rendering"
-    exit 1
-fi
+if [ ! -f /root/firststart ]; then
 
-if [ "$1" = "import" ]; then
-    # Initialize PostgreSQL
     service postgresql start
     sudo -u postgres createuser renderer
     sudo -u postgres createdb -E UTF8 -O renderer gis
@@ -23,16 +13,14 @@ if [ "$1" = "import" ]; then
     # Download Luxembourg as sample if no data is provided
     if [ ! -f /data.osm.pbf ]; then
         echo "WARNING: No import file at /data.osm.pbf, so importing Luxembourg as example..."
-        wget -nv http://download.geofabrik.de/europe/luxembourg-latest.osm.pbf -O /data.osm.pbf
+        wget -nv http://download.geofabrik.de/europe/germany/niedersachsen-latest.osm.pbf -O /data.osm.pbf
     fi
 
     # Import data
     sudo -u renderer osm2pgsql -d gis --create --slim -G --hstore --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua -C 2048 --number-processes ${THREADS:-4} -S /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style /data.osm.pbf
-
-    exit 0
+    touch /root/firststart
 fi
 
-if [ "$1" = "run" ]; then
     # Initialize PostgreSQL and Apache
     service postgresql start
     service apache2 restart
@@ -44,7 +32,3 @@ if [ "$1" = "run" ]; then
     sudo -u renderer renderd -f -c /usr/local/etc/renderd.conf
 
     exit 0
-fi
-
-echo "invalid command"
-exit 1
