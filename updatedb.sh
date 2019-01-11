27fa54c1e459358650cd5113166610e4119ff916
hhansen06 <hhansen06@googlemail.com>
2019-01-11 23:05:05 +0100

    Update updatedb.sh

--- updatedb.sh
+++ updatedb.sh
@@ -8,6 +8,8 @@ fi
 
 if [ ! -f  $WORKOSM_DIR/configuration.txt ]; then
         osmosis --read-replication-interval-init workingDirectory=$WORKOSM_DIR
+        sed -i 's!baseUrl=http://planet.openstreetmap.org/!baseUrl=https://planet.openstreetmap.org/!' $WORKOSM_DIR/configuration.txt
+
 fi
 
 
@@ -21,9 +23,14 @@ WORKOSM_DIR=/home/renderer/osmosis_workdir
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-osmosis -v --read-replication-interval workingDirectory="${WORKOSM_DIR}" --simplify-change --write-xml-change $WORKOSM_DIR/pipline.osc 
-/home/renderer/src/regional/trim_osc.py -d gis -p "${WORKOSM_DIR}/shape.poly" $WORKOSM_DIR/pipline.osc $WORKOSM_DIR/pipline.nds.osc
-osm2pgsql -v --append -s -C 300 -G --hstore --style /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua -d gis $WORKOSM_DIR/pipline.nds.osc
+osmosis --read-replication-interval workingDirectory="${WORKOSM_DIR}" --simplify-change --write-xml-change $WORKOSM_DIR/pipline.osc
+
+/usr/bin/python /home/renderer/src/regional/trim_osc.py -d gis -p "${WORKOSM_DIR}/shape.poly" $WORKOSM_DIR/pipline.osc $WORKOSM_DIR/pipline.nds.osc
+
+osm2pgsql --append -s -e15-20  -o $WORKOSM_DIR/expired_shapes  -C 300 -G --hstore --style /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style -tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua -d gis $WORKOSM_DIR/pipline.nds.osc
+
+render_expired --min-zoom=10 --delete-from=10 --map=ajt < $WORKOSM_DIR/expired_shapes
 
 rm $WORKOSM_DIR/pipline.osc
 rm $WORKOSM_DIR/pipline.nds.osc
+rm $WORKOSM_DIR/expired_shapes
